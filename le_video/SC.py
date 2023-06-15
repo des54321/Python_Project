8a29086d7ba2c9cb4928b1525884c5a9f1b08b52
des54321 <desmondholman2@gmail.com>
2023-06-15 16:00:37 -0700

    adding SC

---
+++ le_video/SC.py
@@ -0,0 +1,79 @@
+import pygame as pg
+from pygame import Vector2
+from menu_engine import Menu, Text
+
+
+pressed_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'COMMA', '1', '2', '3','4', '5', '6', '7', '8', '9', '0']
+pressed = []
+last = []
+
+for i in pressed_letters:
+    pressed.append(False)
+    last.append(False)
+
+
+def update_pressed():
+    global last
+    global pressed
+    global pressed_letters
+    for x, i in enumerate(pressed_letters):
+        test = eval('pg.K_' + i)
+        press = key_down(test)
+        if press and (not last[x]):
+            pressed[x] = True
+        else:
+            pressed[x] = False
+        if press:
+            last[x] = True
+        else:
+            last[x] = False
+
+
+def key_press(key: str):
+    return pressed[pressed_letters.index(key)]
+
+
+def key_down(key: pg.key) -> bool:
+    return pg.key.get_pressed()[key]
+
+def pos_scr(pos):
+    return Vector2(pos[0]+(sw/2),sh-(pos[1]+(sh/2)))
+
+
+def square_step():
+    global squares
+
+
+
+
+
+
+pg.init()
+sw = 1920
+sh = 1080
+screen = pg.display.set_mode((sw, sh), pg.FULLSCREEN)
+
+squares = []
+square_size = []
+
+
+fps_clock = pg.time.Clock()
+fps = 60
+running = True
+while running:
+    events = pg.event.get()
+    for event in events:
+        if event.type == pg.QUIT:
+            running = False
+
+    
+    update_pressed()
+    screen.fill((20,30,15))
+    if key_down(pg.K_BACKSPACE):
+        running = False
+    
+
+
+    pg.display.update()
+    fps_clock.tick(fps)
+pg.quit()

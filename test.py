96036cd6334138c2da12f93e0371baa011b23ab1
des54321 <desmondholman2@gmail.com>
2023-06-26 17:09:24 -0700

    none

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pygame as pg
 from pygame import Vector2
 
 
+
 pressed_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'COMMA', '1', '2', '3','4', '5', '6', '7', '8', '9', '0']
 pressed = []
 last_letters = []
